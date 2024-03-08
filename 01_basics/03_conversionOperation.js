4c7060ee6a9131cbf56dfae41f01d46f855c1314
Suman Majee <78966433+sumanmajee@users.noreply.github.com>
2024-03-08 11:41:11 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33ab"
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33ab"=>NaN
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn = "suman"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//""=>false
+//"suman"=>true
+
+let some=33
+let stringNumber = String(some)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
