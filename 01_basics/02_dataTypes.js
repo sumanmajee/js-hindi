2b0d2048ee931c912ee5ae359a399a739451defe
Suman Majee <78966433+sumanmajee@users.noreply.github.com>
2024-03-08 11:25:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS as newer version
+
+//alert(3 + 3) //we are using nodeJS not browser
+
+let Name="suman"
+let age = 23
+let isLoggedIn = false
+
+//number
+//bigint
+//string
+//boolean
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof Name);
