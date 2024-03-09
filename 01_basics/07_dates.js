2a101af06e94a5aa9d61012550be253f1578bd52
Suman Majee <78966433+sumanmajee@users.noreply.github.com>
2024-03-09 18:58:18 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
