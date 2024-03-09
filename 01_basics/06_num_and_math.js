42ff3ddb471d9eac7f80b90493d88a991eb5bb71
Suman Majee <78966433+sumanmajee@users.noreply.github.com>
2024-03-09 18:25:30 +0000

    numbers

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.89798
+// console.log(otherNumber.toPrecision(3));
+
+
+// *****************Math********************
+
+// console.log(Math.random());
+
+min = 10
+max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
